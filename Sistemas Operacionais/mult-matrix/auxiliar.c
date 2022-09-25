aeb5f8b5b5ca4a6a9339f75fd3bf46a4d0bfc332
Victor Hugo <victor.hugo@quark.tec.br>
2022-09-25 00:21:36 -0300

    Atualizada a função de criação das matrizes e a forma de recebimento e leitura das matrizes na implementação sequencial

--- Sistemas Operacionais/mult-matrix/auxiliar.c
+++ Sistemas Operacionais/mult-matrix/auxiliar.c
@@ -15,63 +15,65 @@ void imprimeMatriz(int **matriz, int linhas, int colunas)
 int main(int argc, char const *argv[])
 {
   // Matriz A
-  int **matriz_A;
-  int linhas_A = atoi(argv[1]);
-  int colunas_A = atoi(argv[2]);
-  matriz_A = malloc(sizeof(int *) * linhas_A);
-  for (int i = 0; i < linhas_A; i++) {
-    matriz_A[i] = malloc(sizeof(int *) * colunas_A);
+  int **matriz_a;
+  int linhas_a = atoi(argv[1]);
+  int colunas_a = atoi(argv[2]);
+  matriz_a = malloc(sizeof(int *) * linhas_a);
+  for (int i = 0; i < linhas_a; i++) {
+    matriz_a[i] = malloc(sizeof(int *) * colunas_a);
   }
 
   // Matriz B
-  int **matriz_B;
-  int linhas_B = atoi(argv[3]);
-  int colunas_B = atoi(argv[4]);
-  matriz_B = malloc(sizeof(int *) * linhas_B);
-  for (int i = 0; i < linhas_B; i++) {
-    matriz_B[i] = malloc(sizeof(int *) * colunas_B);
+  int **matriz_b;
+  int linhas_b = atoi(argv[3]);
+  int colunas_b = atoi(argv[4]);
+  matriz_b = malloc(sizeof(int *) * linhas_b);
+  for (int i = 0; i < linhas_b; i++) {
+    matriz_b[i] = malloc(sizeof(int *) * colunas_b);
   }
 
   // Preenchimento aleatório de matrizes
-  for (int i = 0; i < linhas_A; i++) {
-    for (int j = 0; j < colunas_A; j++) {
-      matriz_A[i][j] = rand() % 10;
+  for (int i = 0; i < linhas_a; i++) {
+    for (int j = 0; j < colunas_a; j++) {
+      matriz_a[i][j] = rand() % 10;
     }
   }
 
-  for (int i = 0; i < linhas_B; i++) {
-    for (int j = 0; j < colunas_B; j++) {
-      matriz_B[i][j] = rand() % 10;
+  for (int i = 0; i < linhas_b; i++) {
+    for (int j = 0; j < colunas_b; j++) {
+      matriz_b[i][j] = rand() % 10;
       printf(" ");
     }
     printf(" \n ");
   }
 
-  FILE *arquivo_matriz;
-  arquivo_matriz = fopen("matrizes.txt", "w");
+  FILE *arquivo_matriz_a, *arquivo_matriz_b;
+  arquivo_matriz_a = fopen("matriz_a.txt", "w");
+  arquivo_matriz_b = fopen("matriz_b.txt", "w");
 
   // Escrevendo as linhas e colunas da matriz
-  fprintf(arquivo_matriz, "%d %d\n", linhas_A, colunas_A);
+  fprintf(arquivo_matriz_a, "%d %d\n", linhas_a, colunas_a);
 
   // Escrevendo os valores da matriz
-  for (int i = 0; i < linhas_A; i++) {
-    for (int j = 0; j < colunas_A; j++) {
-      fprintf(arquivo_matriz, "a%d%d ", i+1, j+1);
-      fprintf(arquivo_matriz,"%d \n", matriz_A[i][j]);
+  for (int i = 0; i < linhas_a; i++) {
+    for (int j = 0; j < colunas_a; j++) {
+      fprintf(arquivo_matriz_a, "a %d %d %d\n", i+1, j+1, matriz_a[i][j]);
     }
   }
-  fprintf(arquivo_matriz, "\n");
 
-  fprintf(arquivo_matriz, "%d %d\n", linhas_B, colunas_B);
-  // Escrevendo os valores da matriz
-  for (int i = 0; i < linhas_B; i++) {
-    for (int j = 0; j < colunas_B; j++) {
-      fprintf(arquivo_matriz, "b%d%d ", i+1, j+1);
-      fprintf(arquivo_matriz,"%d \n", matriz_B[i][j]);
+  fprintf(arquivo_matriz_b, "%d %d\n", linhas_b, colunas_b);
+  for (int i = 0; i < linhas_b; i++) {
+    for (int j = 0; j < colunas_b; j++) {
+      fprintf(arquivo_matriz_b, "b %d %d %d\n", i+1, j+1, matriz_b[i][j]);
     }
   }
-  fprintf(arquivo_matriz, "\n");
 
-  fclose(arquivo_matriz);
+  // Liberando as memórias das matrizes e fechando o arquivo
+  for (int i = 0; i < linhas_a; i++) free(matriz_a[i]);
+  free(matriz_a);
+  for (int i = 0; i < linhas_b; i++) free(matriz_b[i]);
+  free(matriz_b);
+  fclose(arquivo_matriz_b);
+
   return 0;
 }
--- Sistemas Operacionais/mult-matrix/sequencial.c
+++ Sistemas Operacionais/mult-matrix/sequencial.c
@@ -4,6 +4,11 @@
 #include <unistd.h>
 #include <sys/time.h>
 
+#define MAX_LINE_LENGTH 10
+
+int **matriz_a, **matriz_b, **matriz_c;
+FILE *arquivo_matriz_a, *arquivo_matriz_b, *arquivo_matriz_c;
+
 void imprimeMatriz(int **matriz, int linhas, int colunas) 
 {
   for (int i = 0; i < linhas; i++) {
@@ -15,77 +20,111 @@ void imprimeMatriz(int **matriz, int linhas, int colunas)
   }
 }
 
+void leMatriz(FILE *arquivo, int **matriz, int linhas, int colunas) {
+  char line[MAX_LINE_LENGTH], ch;
+  int count = 0;
+  int linha, coluna, valor;
+  while (fgets(line, MAX_LINE_LENGTH, arquivo)) {
+    if (count > 0) {
+      sscanf(line, "%*c %d %d %d", &linha, &coluna, &valor);
+      matriz[linha-1][coluna-1] = valor;
+    }
+    count++;
+  }
+}
+
 int main(int argc, char *argv[]) 
 {
-  // Definição de variaveis
-  // Matriz A
-  int **matriz_A;
-  int linhas_A = atoi(argv[1]);
-  int colunas_A = atoi(argv[2]);
-  matriz_A = malloc(sizeof(int *) * linhas_A);
-  for (int i = 0; i < linhas_A; i++) {
-    matriz_A[i] = malloc(sizeof(int *) * colunas_A);
-  }
-
-  // Matriz B
-  int **matriz_B;
-  int linhas_B = atoi(argv[3]);
-  int colunas_B = atoi(argv[4]);
-  matriz_B = malloc(sizeof(int *) * linhas_B);
-  for (int i = 0; i < linhas_B; i++) {
-    matriz_B[i] = malloc(sizeof(int *) * colunas_B);
-  }
-
-  // Preenchimento aleatório de matrizes
-  for (int i = 0; i < linhas_A; i++)
+   // Lendo arquivos das matrizes
+  if ((arquivo_matriz_a = fopen(argv[1], "r")) == NULL) 
   {
-    for (int j = 0; j < colunas_A; j++) 
-    {
-      matriz_A[i][j] = rand() % 10;
-      //printf(" ");
-    }
-    //printf(" \n");
+    printf("Erro! O arquivo 1 não pôde ser aberto.");
+    exit(1);
   }
 
-  for (int i = 0; i < linhas_B; i++) 
+  if ((arquivo_matriz_b = fopen(argv[2], "r")) == NULL) 
   {
-    for (int j = 0; j < colunas_B; j++) 
-    {
-      matriz_B[i][j] = rand() % 10;
-      //printf(" ");
-    }
-    //printf(" \n ");
+    printf("Erro! O arquivo 2 não pôde ser aberto.");
+    exit(1);
   }
-  
-  int matriz_C[linhas_A][colunas_B], aux = 0;
 
-  // Imprime as matrizes definidas
-  /* printf("---------------------------- 1ª Matriz Gerada ---------------------------------\n\n");
-  imprimeMatriz(matriz_A, linhas_A, colunas_A);
-  printf("---------------------------- 2ª Matriz Gerada ---------------------------------\n\n");
-  imprimeMatriz(matriz_B, linhas_B, colunas_B); */
- 
-  if (colunas_A == linhas_B) 
+  // Lendo as dimensões das matrizes
+  int linhas_a, colunas_a, linhas_b, colunas_b;
+  fscanf(arquivo_matriz_a, "%d %d", &linhas_a, &colunas_a);
+  fscanf(arquivo_matriz_b, "%d %d", &linhas_b, &colunas_b);
+
+  // Alocando as matrizes
+  matriz_a = malloc(sizeof(int *) * linhas_a);
+  for (int i = 0; i < linhas_a; i++) {
+    matriz_a[i] = malloc(sizeof(int *) * colunas_a);
+  }
+
+  matriz_b = malloc(sizeof(int *) * linhas_b);
+  for (int i = 0; i < linhas_b; i++) {
+    matriz_b[i] = malloc(sizeof(int *) * colunas_b);
+  }
+
+  matriz_c = malloc(sizeof(int *) * linhas_a);
+  for (int i = 0; i < linhas_a; i++) {
+    matriz_c[i] = malloc(sizeof(int *) * colunas_b);
+  }
+
+  leMatriz(arquivo_matriz_a, matriz_a, linhas_a, colunas_a);
+  leMatriz(arquivo_matriz_b, matriz_b, linhas_b, colunas_b);
+
+  int aux = 0;
+  unsigned long time_diff;
+  if (colunas_a == linhas_b) 
   {
     int x;
     // Processamento e saida em tela  =  PRODUTO DAS MATRIZES
     struct timeval stop, start;
     gettimeofday(&start, NULL);
-    for (int i = 0; i < linhas_A; i++) 
+    for (int i = 0; i < linhas_a; i++) 
     {
-      for (int j = 0; j < colunas_B; j++) 
+      for (int j = 0; j < colunas_b; j++) 
       {
-        matriz_C[i][j] = 0;
-        for (x = 0; x < linhas_B; x++) {
-          aux += matriz_A[i][x] * matriz_B[x][j];
+        matriz_c[i][j] = 0;
+        for (x = 0; x < linhas_b; x++) {
+          aux += matriz_a[i][x] * matriz_b[x][j];
         }
-        matriz_C[i][j] = aux;
+        matriz_c[i][j] = aux;
         aux = 0;
       }
     }
     gettimeofday(&stop, NULL);
-    unsigned long time_diff = ((stop.tv_sec - start.tv_sec) * 1000) + ((stop.tv_usec - start.tv_usec) / 1000.0);
-    printf("\nTempo: %lu [ms]\n", time_diff);
+    time_diff = ((stop.tv_sec - start.tv_sec) * 1000) + ((stop.tv_usec - start.tv_usec) / 1000.0);
+    printf("Tempo: %lu [ms]\n", time_diff);
+    
+    if ((arquivo_matriz_c = fopen("matriz_c.txt", "w")) == NULL) 
+    {
+      printf("Erro! O arquivo 3 não pôde ser aberto.");
+      exit(1);
+    }
+
+    fprintf(arquivo_matriz_c, "%d %d\n", linhas_a, colunas_b);
+    for (int i = 0; i < linhas_a; i++) {
+      for (int j = 0; j < colunas_b; j++) 
+      {
+        fprintf(arquivo_matriz_c, "c%d%d ", i+1, j+1);
+        fprintf(arquivo_matriz_c,"%d \n", matriz_c[i][j]);
+      }
+    }
+    fprintf(arquivo_matriz_c, "%lu\n", time_diff);
+
+    // Liberando as memórias das matrizes
+    for (int i = 0; i < linhas_a; i++) free(matriz_a[i]);
+    free(matriz_a);
+    
+    for (int i = 0; i < linhas_b; i++) free(matriz_b[i]);
+    free(matriz_b);
+
+    for (int i = 0; i < linhas_a; i++) free(matriz_c[i]);
+    free(matriz_c);
+
+    // Fechando os arquivos
+    fclose(arquivo_matriz_a);
+    fclose(arquivo_matriz_b);
   }
 
   else 
@@ -95,13 +134,6 @@ int main(int argc, char *argv[])
     exit(0);
   }
 
-  // Liberando as memórias das matrizes
-  for (int i = 0; i < linhas_A; i++) free(matriz_A[i]);
-  free(matriz_A);
-  
-  for (int i = 0; i < linhas_B; i++) free(matriz_B[i]);
-  free(matriz_B);
-  
   return 0;
 }
 
